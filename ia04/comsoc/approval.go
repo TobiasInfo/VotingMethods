9c93070a89a62e868b091cb23329949c803a81df
Nassim SAIDI <119054397+SAIDI-NASSIM@users.noreply.github.com>
2024-09-22 13:24:35 +0200

    approval logic implemenetd

--- ia04/comsoc/approval.go
+++ ia04/comsoc/approval.go
@@ -1,11 +1,17 @@
 package comsoc
 
-// TODO : Implements these fonctions
 func ApprovalSWF(p Profile, thresholds []int) (count Count, err error) {
 	count = make(Count)
-
+	for i, prefs := range p {
+		if len(prefs) >= thresholds[i] {
+			for _, alt := range prefs {
+				count[alt]++
+			}
+		}
+	}
 	return count, nil
 }
+
 func ApprovalSCF(p Profile, thresholds []int) (bestAlts []Alternative, err error) {
 	count, err := ApprovalSWF(p, thresholds)
 	if err != nil {
